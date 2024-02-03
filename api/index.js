0992fba243a342cc408cd4c3ccaaf2f9c8d84ea1
Nikunj Saraf <nikunjsaraf2001@gmail.com>
2024-02-03 22:04:29 +0530

    add more comments for mongodb connections

--- api/index.js
+++ api/index.js
@@ -1,16 +1,20 @@
-// Import the express module
+// Import the express, mongoose and dotenv modules
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
+// Load environment variables from .env file
 dotenv.config();
 
+// Connect to MongoDB database using the connection string from environment variables
 mongoose
 	.connect(process.env.MONGODB_URL)
 	.then(() => {
+		// Log a success message when database connection is established
 		console.log('Database is connected');
 	})
 	.catch((error) => {
+		// Log the error if database connection fails
 		console.log(error);
 	});
 
