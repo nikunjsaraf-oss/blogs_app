a64c04ae4e462e04a03b35895109dda126cded74
Nikunj Saraf <nikunjsaraf2001@gmail.com>
2024-02-03 22:11:01 +0530

    create user model

---
+++ api/models/user.models.js
@@ -0,0 +1,15 @@
+import { Timestamp } from 'mongodb';
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+	{
+		username: { type: String, unique: true, required: true },
+		email: { type: String, unique: true, required: true },
+		password: { type: String, required: true },
+	},
+	{ timestamps: true }
+);
+
+const user = mongoose.model('User', userSchema);
+
+export default user;
